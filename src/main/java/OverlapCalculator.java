8076654366f1a092054aa545f860fd34e2bb6ea9
Justyna Fornalik <jfornalik@gmail.com>
2018-03-28 12:40:34 +0200

    Add tests and refactor method

--- src/main/java/OverlapCalculator.java
+++ src/main/java/OverlapCalculator.java
@@ -1,19 +1,30 @@
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class OverlapCalculator {
+
     public int calculateOverlap(int a1, int b1, int c1, int d1, int a2, int b2, int c2, int d2 ) {
+        //should I check if all arguments are integers?
         int a;
         int b;
         int c;
         int d;
         int overlap = 0;
 
-        if ((a1 <= a2 && a2 <= c1) || (b1 <= b2 && b2 <= d1) || (a1 <= c2 && c2 <= c1) || (b1 <= d2 && d2 <= d1)) {
+        if ((a1 <= a2 && a2 <= c1) || (b1 <= b2 && b2 <= d1) || (a1 <= c2 && c2 <= c1) || (b1 <= d2 && d2 <= d1) ||
+                (a2 <= a1 && a1 <= c2) || (b2 <= b1 && b1 <= d2) || (a2 <= c1 && c1 <= c2) || (b2 <= d1 && d1 <= d2)) {
 
-            a = a2 <= a1 ? a1 : a2;
+            a = max(a1,a2);
+            b = max(b1, b2);
+            c = min(c1,c2);
+            d = min(d1,d2);
+
+            //first version:
+            /*a = a2 <= a1 ? a1 : a2;
             b = b2 >= b1 ? b2 : b1;
             c = c2 <= c1 ? c2 : c1;
-            d = d2 <= d1 ? d2 : d1;
+            d = d2 <= d1 ? d2 : d1;*/
 
             overlap = abs((d-b)*(a-c));
 
--- src/test/java/OverlapCalculatorTest.java
+++ src/test/java/OverlapCalculatorTest.java
@@ -1,6 +1,57 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OverlapCalculatorTest {
+    private OverlapCalculator overlapCalculator;
+
+    @BeforeEach
+    public void initEach() {
+        this.overlapCalculator = new OverlapCalculator();
+    }
+
+    @Test
+    public void testCalculateOverlapWhenNoOverlap() {
+        int expectedOutput = 0;
+        int actualOutput = overlapCalculator.calculateOverlap(-12, -4, -1, -2, 0,0, 100, 3000);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testCalculateOverlapWhenOverlappingRectangleInsideTouchingSides() {
+        int expectedOutput = 36;
+        int actualOutput = overlapCalculator.calculateOverlap(-1, -1, 10, 10, -1, 0 , 3, 9);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testCalculateOverlapWhenRectanglesTheSame() {
+        int expectedOutput = 121;
+        int actualOutput = overlapCalculator.calculateOverlap(-1,-1, 10, 10, -1, -1, 10, 10);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testCalculateOverlapWhenSecondOverlappingRectangleBigger() {
+        int expectedOutput = 9;
+        int actualOutput = overlapCalculator.calculateOverlap(-1,-1, 10, 10, 2,2,5,5);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testCalculateOverlapWhenNoOverlapButSidesTouching() {
+        int expectedOutput = 0;
+        int actualOutput = overlapCalculator.calculateOverlap(1,0,5,4,5,0,7,2);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testCalculateOverlapWhenOverlappingRectangleInsideNotTouchingSides() {
+        int expectedOutput = 6;
+        int actualOutput = overlapCalculator.calculateOverlap(-4, -1, -2, 2,-6,-3,-1,4);
+        assertEquals(expectedOutput, actualOutput);
+    }
 
 
 }
